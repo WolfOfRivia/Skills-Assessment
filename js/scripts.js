99555fa58ec7c58ab04ac44e2454aad1bf7df957
Adrian Harris <shogunaut90@gmail.com>
2018-09-02 01:41:04 -0500

    Initial Commit for Project

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
-  // write your scripts here
+  var images = [
+    "../assets/HERO-1.jpg",
+    "../assets/HERO-2.jpg",
+    "../assets/HERO-3.jpg"
+  ];
+
+  var bgImage = images[Math.floor(Math.random() * images.length)];
+  $(".hero").css("background", "url(" + bgImage + ")");
+
+  $(window).scroll(function() {
+    var scrollY = window.scrollY;
+
+    if (scrollY > 250) {
+      $(".img-1").addClass("fadeInDown");
+      $(".img-2").addClass("fadeInRight");
+      $(".img-3").addClass("fadeInUp");
+      $(".img-4").addClass("fadeInUp");
+      $(".img-5").addClass("fadeInLeft");
+      $(".title").addClass("fadeInDown");
+      $("hr").addClass("fadeInRight");
+      $(".description").addClass("fadeInUp");
+    }
+  });
+
+  console.log(bgImage);
 });
